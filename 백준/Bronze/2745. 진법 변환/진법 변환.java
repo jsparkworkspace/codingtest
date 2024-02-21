cdd06775282357b76f501691d580c13ba4e8f448
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-21 21:12:34 +0900

    [Bronze II] Title: 진법 변환, Time: 208 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String n = sc.next();
+    int m = sc.nextInt();
+
+    System.out.print(Integer.parseInt(n, m));
+
+
+    sc.close();
+
+  }
+
+}
