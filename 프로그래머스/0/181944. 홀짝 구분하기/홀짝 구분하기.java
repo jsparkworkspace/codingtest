a6ddba612a83ca5a20d5eca2b42f36e454f87f83
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-22 09:52:14 +0900

    [level 0] Title: 홀짝 구분하기, Time: 223.68 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -5,11 +5,7 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        if (n % 2 == 0) {
-            System.out.printf("%d is even", n);
-        } else {
-            System.out.printf("%d is odd", n);
-        }
+        System.out.print(n + " is " + (n % 2 == 0 ? "even" : "odd"));
         
         sc.close();
     }
