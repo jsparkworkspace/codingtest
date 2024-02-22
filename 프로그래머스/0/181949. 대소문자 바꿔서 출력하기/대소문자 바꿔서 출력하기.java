b2bdb30ef89f1f8d8668c2d0954f23150fc95c9a
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-22 09:37:57 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 148.91 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) >= 'a' && a.charAt(i) <= 'z') {
+                System.out.printf("%c",a.charAt(i) + 'A' - 'a');
+            } else if (a.charAt(i) >= 'A' && a.charAt(i) <= 'Z') {
+                System.out.printf("%c", a.charAt(i) + 'a' - 'A');
+            }
+        }
+        sc.close();
+    }
+}
