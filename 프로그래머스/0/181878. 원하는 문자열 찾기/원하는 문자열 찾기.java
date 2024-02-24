3252fe1dda6e20ed47696480376006e552c4c3a2
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-24 16:29:04 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 4.45 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,16 +1,5 @@
 class Solution {
     public int solution(String myString, String pat) {
-        int answer = 0;
-        myString = myString.toLowerCase();
-        pat = pat.toLowerCase();
-        for (int i = 0; i < myString.length() - pat.length() + 1; i++) {
-            String str = "";
-            for (int j = i; j < i+ pat.length(); j++) {
-                str += myString.charAt(j);
-            }
-            answer = str.equals(pat) ? 1 : 0;
-            if (answer == 1) break;
-        }
-        return answer;
+        return myString.toLowerCase().contains(pat.toLowerCase()) ? 1 : 0;
     }
 }
