d28fbc727c4526e2fb4dfd4fa9b0930b735d1359
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-24 16:05:56 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        
+        int mul = 1;
+        int sum = 0;
+        for (int a : num_list) {
+            mul *= a;
+            sum += a;
+        }
+        
+        return mul < sum * sum ? 1 : 0;
+    }
+}
