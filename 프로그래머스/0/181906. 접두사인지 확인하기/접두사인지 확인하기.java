3e1825c7dfb4c6b821a51bca771e6f065598b998
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-24 17:25:25 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        int answer = 0;
-        String str = "";
-        for (int i = 0; i < my_string.length(); i++) {
-            str += my_string.charAt(i);
-            answer = is_prefix.equals(str) ? 1 : 0;
-            if (answer == 1) break;
-        }
-        return answer;
+        return my_string.indexOf(is_prefix) == 0 ? 1 : 0;
     }
 }
