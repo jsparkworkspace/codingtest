e399e25f81015d28f9687b7c8d93a885af4b2861
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-26 18:22:22 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,28 +1,17 @@
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        if (arr1.length > arr2.length) {
-            return 1;
-        } else if (arr1.length < arr2.length) {
-            return -1;
-        } else if (arr1.length == arr2.length) {
-            int sum1 = 0;
-            int sum2 = 0;
+        int answer = Integer.compare(arr1.length, arr2.length);
+        
+        if (answer == 0) {
+            int sum1 = 0; int sum2 = 0;
             for (int i : arr1) {
                 sum1 += i;
             }
-            
             for (int i : arr2) {
                 sum2 += i;
             }
-            
-            if (sum1 > sum2) {
-                return 1;
-            } else if (sum1 < sum2) {
-                return -1;
-            } else if (sum1 == sum2) {
-                return 0;
-            }
+            answer = Integer.compare(sum1, sum2);
         }
-        return -1;
+        return answer;
     }
 }
